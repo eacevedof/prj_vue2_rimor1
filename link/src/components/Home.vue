b8a61e99456ede74c053fbf5fe19720614969729
Eduardo A.F <eacevedof@yahoo.es>
2018-10-27 11:39:26 +0200

    migrando listado de https://github.com/eacevedof/prj_vue2_rimor1/blob/master/vue2/video12/component.html

--- link/src/components/Home.vue
+++ link/src/components/Home.vue
@@ -1,17 +1,75 @@
 <template>
+  <!--home.vue-->
   <div>
     <h1>{{msg}}</h1>
-    <p>Mi pagina home</p>
+      <div class="row">
+        <div class="col-sm-4">
+            <h1>Albums</h1>
+            <eaf-list v-bind:aritems="albums"/>
+        </div>
+        <div class="col-sm-4">
+            <h1>Posts</h1>
+            <eaf-list v-bind:aritems="posts"/>
+        </div>      
+        <div class="col-sm-4">
+            <h1>Photos</h1>
+            <eaf-list v-bind:aritems="photos"/>
+        </div>                  
+        <div class="col-sm-12">
+            <h1>JSON</h1>
+            <pre style="background:#e0e0e0; border:1px solid #ccc">
+                {{ $data }}
+            </pre>
+        </div>
+      </div>
   </div>
+  <!--/home.vue-->
 </template>
 
 <script>
+//https://github.com/eacevedof/prj_vue2_rimor1/blob/master/vue2/video12/component.html
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+//Vue.use(VueAxios, axios)
+
 export default {
   name: 'Home',
-  data () {
-    return {
-      msg: "Bienvenido"
-    }
-  }
+
+    created: function () {
+        this.posts = this.get_posts()
+        this.albums = this.get_albums()
+    },
+
+    data(){
+      return {
+        msg: "Bienvenido",
+        posts: [],
+        albums: [],
+        photos: [
+            {title: "foto 1"},
+            {title: "foto 2"}
+        ]
+      }
+    },//data
+    
+    methods:{
+        get_posts : function() {
+            const sUrlJson = "https://jsonplaceholder.typicode.com/posts"
+            axios.get(sUrlJson).then((response)=>{
+                this.posts = response.data
+            })
+        },//get_posts
+
+        get_albums : function() {
+            const sUrlJson = "https://jsonplaceholder.typicode.com/albums"
+            axios.get(sUrlJson).then((response)=>{
+                this.albums = response.data
+            })
+        },//get_albums        
+    },//methods
+
+    computed: {
+
+    }//computed
 }
 </script>
