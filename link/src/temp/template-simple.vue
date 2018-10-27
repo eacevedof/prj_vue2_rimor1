1166dec42d88e30d4150b44ba986ffca1570ba55
Eduardo A.F <eacevedof@yahoo.es>
2018-10-27 17:03:57 +0200

    template-simple

---
+++ link/src/temp/template-simple.vue
@@ -0,0 +1,38 @@
+<template>
+    <!--template-simple.vue-->
+    <div>
+        <h1>template-simple</h1>
+        <p> 
+        - template-simple.vue -
+        </p>
+    </div>
+    <!--/template-simple.vue-->
+</template>
+
+<script>
+export default {
+    name: 'template-simple',
+
+    components : {
+    },
+
+    created: function () {
+        console.log("create()")
+    },//created()
+
+    data(){
+        console.log("data()")
+        return {
+            title: "- template-simple.vue -",
+        }
+    },//data()
+    
+    methods:{
+
+    },//methods
+
+    computed: {
+
+    }//computed
+}//template-simple.
+</script>
