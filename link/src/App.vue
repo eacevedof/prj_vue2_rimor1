87e1e614afa1e755627027b067b72d276e4891f4
Eduardo A.F <eacevedof@yahoo.es>
2018-10-27 20:59:32 +0200

    instalo bootstrap como módulo

--- link/src/App.vue
+++ link/src/App.vue
@@ -11,6 +11,10 @@
 </template>
 
 <script>
+//https://stackoverflow.com/questions/42684661/adding-bootstrap-to-vue-cli-project
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import eafnavtop from '@/elements/Eafnavtop'
 export default {
 	name: 'App',
@@ -20,5 +24,3 @@ export default {
 }
 </script>
 
-<style>
-</style>
