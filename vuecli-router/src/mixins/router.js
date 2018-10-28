f49770195500230bd9d20ea733571d79d5107f36
Eduardo A.F <eacevedof@yahoo.es>
2018-10-28 12:18:12 +0100

    refactor views nombres archivos

--- vuecli-router/src/mixins/router.js
+++ vuecli-router/src/mixins/router.js
@@ -2,23 +2,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Home from '@/views/Home'
-import Posts from '@/views/Posts'
-import Albums from '@/views/Albums'
-import Photos from '@/views/Photos'
+import Home from '@/views/view_home'
+import Posts from '@/views/view_posts'
+import Albums from '@/views/view_albums'
+import Photos from '@/views/view_photos'
 
 Vue.use(VueRouter)
 
 const arRoutes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/posts', name: 'Posts', component: Posts },
-  { path: '/albums', name: 'Albums', component: Albums },
-  { path: '/photos', name: 'Photos', component: Photos }
+    { path: '/', name: 'Home', component: Home },
+    { path: '/posts', name: 'Posts', component: Posts },
+    { path: '/albums', name: 'Albums', component: Albums },
+    { path: '/photos', name: 'Photos', component: Photos }
 ]
 
 const oVueRouter = new VueRouter({
-  mode: 'history',
-  routes: arRoutes
+    mode: 'history',
+    routes: arRoutes
 })// oVueRouter
 
 export default oVueRouter
