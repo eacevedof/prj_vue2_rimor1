6d60caebd1ce54660c76db203a2a20bb1e49a587
Eduardo A.F <eacevedof@yahoo.es>
2018-10-28 12:29:16 +0100

    src/main.js

--- vuecli-router/src/main.js
+++ vuecli-router/src/main.js
@@ -1,9 +1,9 @@
-//
+//src/main.js
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './mixins/router'
+import App from '@/App'
+import router from '@/mixins/router'
 import 'bootstrap'
 import Pagetitle from '@/mixins/pagetitle'
 
