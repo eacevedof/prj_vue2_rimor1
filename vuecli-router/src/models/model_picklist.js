0fba504d4b70cc3648a7aaffcfe2a1fc01987cb0
Eduardo A.F <eacevedof@yahoo.es>
2018-12-01 15:06:03 +0100

    config.js

--- vuecli-router/src/models/model_picklist.js
+++ vuecli-router/src/models/model_picklist.js
@@ -1,16 +1,15 @@
 //ModelPicklist
-import config from '../../static/config.json'
 import axios from 'axios'
 
 export default {
 
     get_titles : function(fnLoad) {
-        let urldata = config.domain+'picklists/titles'
+        let urldata = oConfig.endpoint+'picklists/titles'
         axios.get(urldata).then(fnLoad)
     },//get_titles()
 
     get_departments : function(fnLoad) {
-        let urldata = config.domain+'picklists/departments'
+        let urldata = oConfig.endpoint+'picklists/departments'
         axios.get(urldata).then(fnLoad)
     },//get_departments()    
 
--- vuecli-router/src/models/model_post.js
+++ vuecli-router/src/models/model_post.js
@@ -1,22 +1,21 @@
 //ModelPost
-import config from '../../static/config.json'
 import axios from 'axios'
 
 export default {
    
     get_data(fnLoad,id) {
         console.log("page-id:"+id)
-        let urldata = config.domain+'employees?page='+id
+        let urldata = oConfig.endpoint+'employees?page='+id
         axios.get(urldata).then(fnLoad)
     },//get_data()
 
     get_profile(oData,fnResult) {
-        let urldata = config.domain+'employees/profile?id='+oData.id
+        let urldata = oConfig.endpoint+'employees/profile?id='+oData.id
         axios.post(urldata,oData).then(fnResult)
     },//get_profile()    
 
     insert(oData,fnResult) {
-        let urldata = config.domain+'employees/insert'
+        let urldata = oConfig.endpoint+'employees/insert'
         const oForm = new FormData()
 
         for(let sKey in oData)
